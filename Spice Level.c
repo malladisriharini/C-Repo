57ee1b089773b5b000ac136eabf2f4734d1b5d6e
malladisriharini <malladisriharini@gmail.com>
2024-10-12 13:34:07 +0530

    Uploading code: Spice Level.c

---
+++ Spice Level.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<4){
+        printf("MILD");
+    }
+    else if(x>=4 && x<7){
+        printf("MEDIUM");
+    }
+    else{
+        printf("HOT");
+    }
+    
+}
