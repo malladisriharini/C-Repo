f3ed0a120e38e3a267117481dda9bddf59ca5230
malladisriharini <malladisriharini@gmail.com>
2024-12-13 00:38:47 +0530

    Uploading code: Lowercase Letters in a String .c

---
+++ Lowercase Letters in a String .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    scanf("%s",str);
+    int count=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='a' && str[i]<='z'){
+            count++;
+        }
+    }
+    printf("%d",count);
+}
